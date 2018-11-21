90ab0bf80293523223fbe7b0b2c8ca31db3cd157
Oba-One <obaone01@gmail.com>
2018-11-21 13:32:52 -0800

    created build for deployment on google app engine.

---
+++ build/precache-manifest.99471083c0b44fcbb1828abe07dae636.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4de430dde2298e5af2f8287318acf19f",
+    "url": "/static/media/nasaLogo.4de430dd.png"
+  },
+  {
+    "revision": "4740de47738d23c2a0c2066e2b18451e",
+    "url": "/static/media/marsSurface.4740de47.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "237a2dccb4dff80c13c0",
+    "url": "/static/js/main.237a2dcc.chunk.js"
+  },
+  {
+    "revision": "63459668129d400f68ba",
+    "url": "/static/js/1.63459668.chunk.js"
+  },
+  {
+    "revision": "237a2dccb4dff80c13c0",
+    "url": "/static/css/main.ee1be820.chunk.css"
+  },
+  {
+    "revision": "63459668129d400f68ba",
+    "url": "/static/css/1.43195717.chunk.css"
+  },
+  {
+    "revision": "3fd2013f9a912f680d2097079671e56e",
+    "url": "/index.html"
+  }
+];
